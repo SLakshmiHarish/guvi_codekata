56762da2938e98a0a5e2cc7236096fc01d3205e3
SLakshmiHarish <40816037+SLakshmiHarish@users.noreply.github.com>
2018-07-07 09:49:35 +0530

    Create harish21.py

---
+++ harish21.py
@@ -0,0 +1,10 @@
+def reverse(string):
+    string = "".join(reversed(string))
+    return string
+ 
+s = "harish"
+ 
+print ("The original string  is : s")
+print (s)
+print ("The reversed string(using reversed) is : s")
+print (reverse(s))
