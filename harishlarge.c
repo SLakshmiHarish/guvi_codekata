5c40b1c4525fd1aaaa3574c5d7b38331d58ed728
SLakshmiHarish <40816037+SLakshmiHarish@users.noreply.github.com>
2018-07-13 11:39:28 +0530

    Create harishlarge.c

---
+++ harishlarge.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int , num1,num2,num3,char;
+ printf("enter the three numbers",);
+ scanf("%d","%d","%d",&num1,&num2,&num3):
+ if (num1>=num2 && num1>>=num3)
+  printf("this is the largest one", num1);
+ else if (num2>=num1 && num2>=num3)
+   printf("this is the largest number", num2);
+   else
+   printf("this is the number", num3);
+return 0;
+ }
+ 
+ 
